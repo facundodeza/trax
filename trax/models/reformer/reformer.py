ec539498e4df6588667f5f070e722d78393567d9
facudeza <facudeza@debian.facudeza>
2020-08-16 13:30:04 +0200

    input

--- trax/models/reformer/reformer.py
+++ trax/models/reformer/reformer.py
@@ -689,15 +689,15 @@ def Reformer(input_vocab_size=None,
       # Input: encoder_side_tokens, decoder_side_tokens
       # Copy decoder tokens for use in loss.
 
-      tl.Select([0, 1, 1, 2]),                # tok_e tok_d tok_d vec_e
+      tl.Select([0, 1, 1]),                # tok_e tok_d tok_d vec_e
       tl.Branch([], [tl.PaddingMask(),
                      tl.Fn('Squeeze',
-                           lambda x: jnp.squeeze(x , (1,2)), n_out=1)]),
+                           lambda x: jnp.squeeze(jnp.sum(x, axis=2) , (1,2)), n_out=1)]),
       #                                     # tok_e mask  tok_d vec_e
 
       # Encode.
 
-      tl.Select([3, 1, 2], n_in=4 ),                 # vec_e  mask tok_d
+      #tl.Select([3, 1, 2], n_in=4 ),                 # vec_e  mask tok_d
 
       encoder,                              # vec_e  mask tok_d .....
 
